4653abdfae36ba09a890606c7e2bccbd3515709b
Júlio César <j28010269@gmail.com>
2026-08-26 16:54:35 -0300

    projeto inicial

---
+++ backend-cardapioDigital/src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//middlewares basicos
+app.use(cors());
+app.use(express.json());
+
+//rota de teste
+app.get('/', (req,res)=>{
+    return res.json({message: "API do Restaurante rodando com sucesso!"});
+});
+
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
