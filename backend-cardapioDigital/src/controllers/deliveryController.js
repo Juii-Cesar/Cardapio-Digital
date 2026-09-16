233c600035b063dcc7c3e842a4b4a2401f3cbdb0
Júlio César <j28010269@gmail.com>
2026-09-16 19:02:55 -0300

    modulo de taxa de entrega concluido

--- backend-cardapioDigital/src/controllers/deliveryController.js
+++ backend-cardapioDigital/src/controllers/deliveryController.js
@@ -3,7 +3,7 @@ const db = require("../config/db");
 //Lista os bairros disponiveis (para o cliente selecionar no checkout ou para o admin)
 const getNeighborhood = async (req, res) => {
   try {
-    //se receber true no req.query lista só bairros ativos
+    //se receber apenasAtivo=true no req.query lista só bairros ativos
     const { apenasAtivo } = req.query;
     let query = "SELECT * FROM taxas_entrega ORDER BY nome_bairro ASC";
     if (apenasAtivo === "true") {
@@ -24,7 +24,7 @@ const getNeighborhood = async (req, res) => {
 //Adiciona novo bairro com taxa (função admin)
 const createNeighborhood = async (req, res) => {
   const { nome_bairro, taxa } = req.body;
-  if (!nome_bairo || taxa === undefined || taxa < 0) {
+  if (!nome_bairro || taxa === undefined || taxa < 0) {
     return res.status(400).json({ error: "Nome ou taxa invalidos" });
   }
 
@@ -40,8 +40,40 @@ const createNeighborhood = async (req, res) => {
     });
   } catch (error) {
     console.error("Erro ao adicionar bairro: ", error);
-    return res.status(400).json({ error: "Erro ao adicionar bairro." });
+    return res.status(500).json({ error: "Erro ao adicionar bairro." });
   }
 };
 
-//
+//Atualiza a taxa ou o status de um bairro (função admin)
+const updateNeighborhood = async (req, res)=>{
+  const {id}=  req.params;
+  const {nome_bairro, taxa, ativo}= req.body;
+  
+  try{
+    const result = await db.query(
+      `UPDATE taxas_entrega
+      SET nome_bairro = COALESCE($1, nome_bairro),
+      taxa = COALESCE($2, taxa),
+      ativo = COALESCE($3,ativo)
+      WHERE id =$4
+      RETURNING *`,
+      [nome_bairro,taxa,ativo,id]
+    );
+    if (result.rows.length===0){
+      return res.status(404).json({error: 'Bairro não encontrado.'});
+    }
+    return res.status(200).json({
+      message: 'Bairro atualizado com sucesso! ',
+      bairro: result.rows[0]
+    });
+  }catch (error){
+    console.error('Erro ao atualizar bairro');
+    return res.status(500).json({error:'Erro ao atualizar bairro'});
+  }
+};
+
+module.exports = {
+  getNeighborhood,
+  createNeighborhood,
+  updateNeighborhood
+};
